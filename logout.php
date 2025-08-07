f841c8705993c6b03c91725bb5b0b33b703b8ce3
mathis-fournier <mathis.fournier@laplateforme.io>
2025-08-07 15:58:02 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,10 @@
 <?php
 
-class Logout
-{
-    public function Logout()
-    {
+
         session_start();
         session_unset();
         session_destroy();
         header('Location: index.php');
         exit();
-    }
-}
+    
+
